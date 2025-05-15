558a182e7e362cc6eba98e50cb28f22bb4f87d8c
Ichikawa01 <ryo.i.343826@gmail.com>
2025-05-15 20:33:22 +0900

    bgm、SE、称号画像の追加

---
+++ FlickDojoGame/BGMManager.swift
@@ -0,0 +1,8 @@
+//
+//  BGMManager.swift
+//  FlickDojoGame
+//
+//  Created by 市川涼 on 2025/05/15.
+//
+
+import Foundation
---
+++ FlickDojoGame/SEManager.swift
@@ -0,0 +1,8 @@
+//
+//  SEManager.swift
+//  FlickDojoGame
+//
+//  Created by 市川涼 on 2025/05/15.
+//
+
+import Foundation
